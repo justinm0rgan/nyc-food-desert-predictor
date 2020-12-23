ca407c25dcc9ea024a546bc18fe627941cabec05
Justin Williams <justinmorganwilliams@gmail.com>
2020-12-23 16:37:40 -0500

    added function to create map based on predictions

--- src/modeling_functions.py
+++ src/modeling_functions.py
@@ -61,8 +61,68 @@ def confusion_matrix(estimator, X, y, title, display_labels=['Not Food Desert', 
                               cmap=plt.cm.Blues, 
                               display_labels=display_labels, 
                               ax=ax,
-                              normalize='all')
+                              normalize=normalize)
     ax.set_ylabel('True', fontdict={'fontsize':18})
     ax.set_xlabel('Predicted', fontdict={'fontsize':18})
     ax.tick_params(axis='both', which='major', labelsize=16)
     plt.title(title, fontdict={'fontsize':20})
+
+# function that geospatially visualizes predictions
+def create_choropleth_map_pred(df1, df2, df3, col1, col2, col3, cmap, title, ptcolor):
+    '''Creates a choropleth map with three dataframes for plotting predictions
+    Select df1 and col1 for negative class
+    Select df2 and col2 for positive calss
+    Select df3 and col3 for predicted positive class
+    Enter cmap for desired color palette
+    Enter title for map title and image saved name'''
+
+    # create map figure and axes
+    fig, ax = plt.subplots(figsize=(50,40))
+
+    # call .plot() method on df
+    ax = df1.plot(column = col1, 
+                            cmap=cmap, 
+                            ax=ax,
+                            edgecolor='face',
+                            zorder=1,
+                            alpha=0.5,
+                            missing_kwds={
+                                "color": "lightgrey",
+                            })
+
+    df2.plot(column = col2,
+                            color=ptcolor,
+                            ax=ax,
+                            zorder=3,
+                            alpha=0.65)
+    df3.plot(column = col3,
+                    color='yellow',
+                    ax=ax,
+                    zorder=2)
+    
+    # add annotation
+    ax.annotate("•", xy=(0.78, .895), size=90, xycoords='figure fraction', color='olive')
+    ax.annotate("True Positive", xy=(0.80, .90), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .855), size=90, xycoords='figure fraction', color='yellow')
+    ax.annotate("False Positive", xy=(0.80, .86), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .815), size=90, xycoords='figure fraction', color=ptcolor, alpha=0.65)
+    ax.annotate("Food Desert", xy=(0.80, .82), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .765), size=90, xycoords='figure fraction', color='purple', alpha=0.5)
+    ax.annotate("Not Food Desert", xy=(0.80, .77), size=70, xycoords='figure fraction')
+    
+    ax.annotate("•", xy=(0.78, .725), size=90, xycoords='figure fraction', color='lightgrey')
+    ax.annotate("Missing Values", xy=(0.80, .72), size=70, xycoords='figure fraction')
+    
+    # set map title
+    ax.set_title(label=title,fontdict={'fontsize': 100}, loc='center')
+
+
+    # remove axis surrounding map
+    ax.set_axis_off()
+
+    # save map
+    save_fig(title.lower().replace(' ', '_'), transparent=True)
+    plt.show()
